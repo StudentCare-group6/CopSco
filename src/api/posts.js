5fb8434b30f1ec050239452d57dcd49af26c2b44
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2023-09-29 09:11:16 +0530

    Update posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'http://127.0.0.1:8000';
 axios.defaults.withCredentials = true;
 
 export default axios.create({
