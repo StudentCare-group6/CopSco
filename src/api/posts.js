5c51d647d45f3cde6051c688729f86385a0b2bbf
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2023-10-27 15:50:09 +0530

    Update posts.js

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-const BASE_URL = 'http://localhost:8000';
+import axios from "axios";
+const BASE_URL = "http://localhost:8000";
 // const BASE_URL = 'http://13.233.104.173:8000';
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL:    BASE_URL,
-    withCredentials: true
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
