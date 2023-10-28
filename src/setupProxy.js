1fd8dee89533b223c9f461e66591e53b59e116d9
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2023-10-28 09:17:00 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
     app.use((_, res, next) => {
+      console.log("Setting headers");
       res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
       res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
       next();
