546220fa7950cabf65c1c41edd950fefa1196cc5
Mr. M.A.V Lochana <58838692+vish061427@users.noreply.github.com>
2023-10-30 13:40:13 +0530

    create user

--- src/components/Admin/RegisterModal.jsx
+++ src/components/Admin/RegisterModal.jsx
@@ -13,6 +13,9 @@ import image from '../../images/security.png';
 import Stack from '@mui/material/Stack';
 import { useState } from 'react';
 import { policeDivisions } from "../../components/Traffic police/Constants";
+import useAuth from '../../hooks/useAuth';
+import axios from '../../api/posts'
+import { set } from 'lodash';
 
 
 const style = {
@@ -27,17 +30,76 @@ const style = {
     p: 5,
 };
 
-const userRoles = ['Admin', 'Police Division Operator', 'Control Room Operator'];
+// const userRoles = ['admin', 'police-operator', 'police-division', 'traffic-police'];
+const userRoles =[
+    {
+        value: 'admin',
+        label: 'Administrator',
+    },
+    {
+        value: 'police-operator',
+        label: 'Control Room Operator',
+    },
+    {
+        value: 'police-division',
+        label: 'Police Division Operator',
+    },
+    {
+        value: 'traffic-police',
+        label: 'Traffic Police Officer',
+    }
+]
 
 function ChildModal() {
+    const { auth } = useAuth();
+    const password = auth.pwd;
     const [open, setOpen] = React.useState(false);
+    const [passwordCheckResult, setPasswordCheckResult] = useState(null);
+    const [alertMessage, setAlertMessage] = useState('');
+    const { register, errors, getValues } = useFormContext();
     const handleOpen = () => {
         setOpen(true);
     };
     const handleClose = () => {
         setOpen(false);
     };
-    const { register, errors } = useFormContext();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Check if the entered password matches the expected password
+        const enteredPassword = getValues("pass"); // Assuming you have a 'pass' field in your form
+
+        if (enteredPassword === password) {
+            setPasswordCheckResult(true);
+            console.log(getValues(''));
+            try{
+                // consaxios.post('/register', 
+                // JSON.stringify({ policeid: getValues('policeId'), userrole: getValues('userRole')}),
+                // {
+                //   headers: { 'Content-Type': 'application/json' },
+                //   withCredentials: true
+                // },);
+                const response = axios.post('admin/createUsers',
+                JSON.stringify({ policeid: getValues('policeId'), userrole: getValues('userRole')}),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                },);
+                console.log(response.data);
+                setAlertMessage('Password is correct. User registered.');
+                window.location.reload();
+            }catch(err){
+                console.log(err);
+            }
+            
+            // Add your registration logic here
+        } else {
+            setPasswordCheckResult(false);
+            setAlertMessage('Password is incorrect. User registration failed.');
+       
+        }
+    };
+
 
     return (
         <React.Fragment>
@@ -48,7 +110,7 @@ function ChildModal() {
                 aria-labelledby="child-modal-title"
                 aria-describedby="child-modal-description"
             >
-                <Box sx={{ ...style, width: 400, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <Box component='form' onSubmit={handleSubmit} sx={{ ...style, width: 400, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                     <Grid item xs={12} sm={12} sx={{ mt: 3 }}>
                         <Typography align="center" variant='h6' sx={{ fontWeight: '600' }} color='#020617'>
                             Admin Password Required
@@ -70,8 +132,13 @@ function ChildModal() {
                             })}
                         />
                     </Grid>
+                    {passwordCheckResult !== null && (
+                        <Alert severity={passwordCheckResult ? 'success' : 'error'} sx={{ mt: '10px' }}>
+                            {alertMessage}
+                        </Alert>
+                    )}
                     <Grid item xs={12} sm={12} sx={{ mt: 3 }}>
-                        <Button onClick={handleClose} fullWidth>Confirm</Button>
+                        <Button type = 'submit' fullWidth>Confirm</Button>
                     </Grid>
                 </Box>
             </Modal>
@@ -84,22 +151,17 @@ export default function RegisterModal() {
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const { register, errors } = useFormContext();
+    const { register, errors, getValues, setValue } = useFormContext();
     const [role, setRole] = React.useState('');
-    const [selectedDivision, setSelectedDivision] = useState([]); // State to hold selected offences
-    const [selectedDivisionCode, setSelectedDivisionCode] = useState(""); // State to hold selected offences
-    const handleDivisionChange = (event) => {
-        const selectedValue = event.target.value;
-        setSelectedDivision(selectedValue);
-
-        const selectedDivisionCode = policeDivisions.get(selectedValue);
-        setSelectedDivisionCode(selectedDivisionCode);
-    };
-
     const handleChange = (event) => {
         setRole(event.target.value);
+        setValue('userRole', event.target.value);
     };
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(getValues());
+    };
     return (
         <div>
             <Button onClick={handleOpen} startIcon={<UserIcon />} variant='contained'>Create User</Button>
@@ -109,7 +171,7 @@ export default function RegisterModal() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
+                <Box sx={style} component='form' onSubmit={handleSubmit}>
                     <Grid container spacing={1}>
                         <Grid item xs={12} sm={12}>
                             <Typography variant="h6" color='#020617' component="h2" align="center" sx={{ fontWeight: '600' }}>
@@ -133,53 +195,25 @@ export default function RegisterModal() {
                             />
                             {errors.email?.message ? <Alert sx={{ mt: '10px' }} severity="error">{errors.email?.message}</Alert> : ""}
                         </Grid>
-  
-                        <Grid item xs={12} sx={{ mt: 3 }}>
-                            <TextField
-                                id="division"
-                                margin="normal"
-                                label="Division"
-                                select
-                                fullWidth
-                                SelectProps={{
-                                    sx: {
-                                        height: "50px",
-                                    },
-                                    value: selectedDivision,
-                                    onChange: handleDivisionChange,
-                                }}
-                                {...register("divisionTitle", {
-                                    required: "field required",
-                                })}
-                            >
-                                {Array.from(policeDivisions.keys()).map((division) => (
-                                    <MenuItem key={division} value={division}>
-                                        {division}
-                                    </MenuItem>
-                                ))}
-                            </TextField>
-                            {errors.division?.message ? (
-                                <Alert sx={{ mt: "10px" }} severity="error">
-                                    {errors.division?.message}
-                                </Alert>
-                            ) : (
-                                ""
-                            )}
-                        </Grid>
                         <Grid item xs={12} sx={{ mt: 3 }}>
                             <TextField label='User Role' select value={role} onChange={handleChange} fullWidth SelectProps={{
                                 sx: {
                                     height: '50px',
                                 },
                             }}>
-                                {userRoles.map((value) => (
-                                    <MenuItem key={value} value={value}>{value}</MenuItem>
+                                {userRoles.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
                                 ))}
                             </TextField>
                         </Grid>
                         <Grid item xs={12} sx={{ mt: 3 }}>
                             <ChildModal />
                         </Grid>
+                        <Button type='submit'>
+                            Submit
+                        </Button>
                     </Grid>
 
                 </Box>
