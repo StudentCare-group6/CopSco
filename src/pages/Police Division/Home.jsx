a5610c4a8de5ec721bb841e65dae7cc3a28d1888
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2023-10-31 07:18:06 +0530

    video verification changes for division

--- src/pages/Police Division/Home.jsx
+++ src/pages/Police Division/Home.jsx
@@ -1,23 +1,135 @@
-import React from "react";
-import VideoSamples from "../../components/Police Division/Home/VideoSamples"
-import PaginationRounded from "../../components/Police Division/Home/Pagination";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Tab, Tabs } from "@mui/material";
+import Badge from "@mui/material/Badge";
+import { useState } from "react";
+import Popup from "../../components/General user/video_upload/Popup";
+import Stack from "@mui/material/Stack";
+import AcceptedTable from "../../components/Police Division/Home/AcceptedTable";
+import PendingTable from "../../components/Police Division/Home/PendingTable";
+import RejectedTable from "../../components/Police Division/Home//RejectedTable";
+import { useEffect } from "react";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import useFineContext from "../../hooks/useFineContext";
+import axios from '../../api/posts';
+import useAuth from '../../hooks/useAuth';
 
-export default function Home() {
-  
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <>
-      <div className="flex-auto overflow-hidden w-[99%]">
-        <div className="flex flex-col h-full">
-          <div className="overflow-y-auto">
-            <div className="max-h-[calc(100vh-100px)] w-[99%]">
-              <VideoSamples />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="ml-[38%] mt-10">
-        <PaginationRounded />
-      </div>
-    </>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
   );
 }
+
+export default function UploadPage() {
+
+  const [value, setValue] = useState(0);
+  const {auth} = useAuth();
+  const userData = {
+    police_username: 43956,
+  };
+  const { acceptedUploads, rejectedUploads, pendingUploads, setAcceptedUploads, setRejectedUploads, setPendingUploads } = useFineContext();
+  const getUploads = async () => {
+    
+    try {
+      const response = await axios.get("police-division/viewVerifiedVideos", { params: userData });
+      console.log(response.data);
+      for (let i = 0; i < response.data.videos.length; i++) {
+        const upload = response.data.videos[i];
+        // console.log(upload);
+        // const isDuplicate = pendingUploads.some((item) => item.id === upload.id) || acceptedUploads.some((item) => item.id === upload.id) || rejectedUploads.some((item) => item.id === upload.id);
+        // if (!isDuplicate) {
+        //   if (upload.status === "Pending Review") {
+        //     setPendingUploads((prevPendingUploads) => [...prevPendingUploads, upload]);
+        //   } else if (upload.status === "Accepted") {
+        //     setAcceptedUploads((prevAcceptedUploads) => [...prevAcceptedUploads, upload]);
+        //   } else if (upload.status === "Rejected") {
+        //     setRejectedUploads((prevRejectedUploads) => [...prevRejectedUploads, upload]);
+        //   } else {
+        //     console.log("Error in getting uploads");
+        //   }
+        // }
+        setAcceptedUploads((prevAcceptedUploads) => [...prevAcceptedUploads, upload]);
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUploads();
+  }, []);
+
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, height: "100vh" }}>
+        <Box className="py-6">
+          <Stack direction="row" justifyContent="space-between">
+            <h2 className="text-3xl font-bold">Manage your Uploads</h2>
+            <Popup />
+          </Stack>
+
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            sx={{ borderColor: "divider" }}
+            centered
+          >
+            <Tab
+              label={
+                <Badge badgeContent={0} color="primary">
+                  Accepted
+                </Badge>
+              }
+              sx={{ fontWeight: "bold" }}
+            />
+
+            <Tab
+              label={
+                <Badge badgeContent={0} color="primary">
+                  Pending Review
+                </Badge>
+              }
+              sx={{ fontWeight: "bold" }}
+            />
+
+            <Tab
+              label={
+                <Badge badgeContent={0} color="primary">
+                  Rejected
+                </Badge>
+              }
+              sx={{ fontWeight: "bold" }}
+            />
+          </Tabs>
+          <TabPanel value={value} index={0} style={{ overflowY: "auto", height: "80vh" }}>
+            <AcceptedTable />
+          </TabPanel>
+          <TabPanel value={value} index={1} style={{ overflowY: "auto", height: "80vh" }}>
+            {/* <PendingTable /> */}
+          </TabPanel>
+          <TabPanel value={value} index={2} style={{ overflowY: "auto", height: "80vh" }}>
+            {/* <RejectedTable /> */}
+          </TabPanel>
+        </Box>
+      </Box>
+    </div>
+  );
+}
+
